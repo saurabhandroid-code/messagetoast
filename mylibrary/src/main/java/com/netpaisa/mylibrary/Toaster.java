dd7f6341f65f5c9505a854230dfacb7f9ae630fc
uttam <uttam.riseintech@gmail.com>
2019-09-12 14:40:39 +0530

    Initial Commit

---
+++ mylibrary/src/main/java/com/netpaisa/mylibrary/Toaster.java
@@ -0,0 +1,13 @@
+package com.netpaisa.mylibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Toaster {
+
+    public static void s(Context c, String message){
+
+        Toast.makeText(c,message, Toast.LENGTH_SHORT).show();
+
+    }
+}
